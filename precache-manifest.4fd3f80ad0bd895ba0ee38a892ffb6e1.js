4a0b6d3288dfd0715ce183173842c1b02486c777
codemunha <codemunha@gmail.com>
2020-10-12 00:37:46 +0700

    Initial dist subtree commit

---
+++ precache-manifest.4fd3f80ad0bd895ba0ee38a892ffb6e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e84e15fd903c772e46",
+    "url": "/vue3-ts/assets/css/app.67447e21.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue3-ts/assets/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "83bd4e83447877759ecb",
+    "url": "/vue3-ts/assets/js/about.6e6ba547.js"
+  },
+  {
+    "revision": "31e84e15fd903c772e46",
+    "url": "/vue3-ts/assets/js/app.26ddf8c1.js"
+  },
+  {
+    "revision": "ac50361bf04b15fdcf64",
+    "url": "/vue3-ts/assets/js/chunk-vendors.df525f85.js"
+  },
+  {
+    "revision": "19ba4385d3a52946de48",
+    "url": "/vue3-ts/assets/js/home.f9894071.js"
+  },
+  {
+    "revision": "7181f41bd5c2f957c226023786903d82",
+    "url": "/vue3-ts/index.html"
+  },
+  {
+    "revision": "e1d8f1e3e0364fc6c5444cf40e538688",
+    "url": "/vue3-ts/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-ts/robots.txt"
+  }
+]);
