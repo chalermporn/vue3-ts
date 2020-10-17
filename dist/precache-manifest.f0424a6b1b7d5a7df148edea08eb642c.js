207878ea23a8abd7c7703a729919feb6614da384
codemunha@gmail.com <codemunha@gmail.com>
2020-10-17 12:19:20 +0700

    update ks8

--- dist/precache-manifest.f0424a6b1b7d5a7df148edea08eb642c.js
+++ dist/precache-manifest.f0424a6b1b7d5a7df148edea08eb642c.js
@@ -1,42 +1,38 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ccc8a5c21a178511f50",
-    "url": "/vue3-ts/assets/css/app.825836b9.css"
+    revision: '31e84e15fd903c772e46',
+    url: '/vue3-ts/assets/css/app.67447e21.css'
   },
   {
-    "revision": "82b9c7a5a3f405032b1db71a25f67021",
-    "url": "/vue3-ts/assets/img/logo.82b9c7a5.png"
+    revision: '82b9c7a5a3f405032b1db71a25f67021',
+    url: '/vue3-ts/assets/img/logo.82b9c7a5.png'
   },
   {
-    "revision": "83bd4e83447877759ecb",
-    "url": "/vue3-ts/assets/js/about.6e6ba547.js"
+    revision: '83bd4e83447877759ecb',
+    url: '/vue3-ts/assets/js/about.6e6ba547.js'
   },
   {
-    "revision": "5ccc8a5c21a178511f50",
-    "url": "/vue3-ts/assets/js/app.26ddf8c1.js"
+    revision: '31e84e15fd903c772e46',
+    url: '/vue3-ts/assets/js/app.26ddf8c1.js'
   },
   {
-    "revision": "ac50361bf04b15fdcf64",
-    "url": "/vue3-ts/assets/js/chunk-vendors.df525f85.js"
+    revision: 'ac50361bf04b15fdcf64',
+    url: '/vue3-ts/assets/js/chunk-vendors.df525f85.js'
   },
   {
-    "revision": "19ba4385d3a52946de48",
-    "url": "/vue3-ts/assets/js/home.f9894071.js"
+    revision: '19ba4385d3a52946de48',
+    url: '/vue3-ts/assets/js/home.f9894071.js'
   },
   {
-    "revision": "d8be9881c1cf8d76e7df9fed28621bc2",
-    "url": "/vue3-ts/img/Capture.PNG"
+    revision: '7181f41bd5c2f957c226023786903d82',
+    url: '/vue3-ts/index.html'
   },
   {
-    "revision": "77e584ac6e05f72d05ba50904f99e206",
-    "url": "/vue3-ts/index.html"
+    revision: 'e1d8f1e3e0364fc6c5444cf40e538688',
+    url: '/vue3-ts/manifest.json'
   },
   {
-    "revision": "e1d8f1e3e0364fc6c5444cf40e538688",
-    "url": "/vue3-ts/manifest.json"
-  },
-  {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/vue3-ts/robots.txt"
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: '/vue3-ts/robots.txt'
   }
-]);
+])
